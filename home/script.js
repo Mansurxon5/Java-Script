0dedb9e264cecbeee3dd66eb404f3c38ddd32d2d
Mansurxon G'iyosiddinov <mansurxongiyosiddinov4543@gmail.com>
2022-11-03 15:32:16 +0500

    end

---
+++ home/script.js
@@ -0,0 +1,4 @@
+alert('JavaScript odatda dastur ob\'ektlariga dasturiy kirish uchun o\'rnatilgan til sifatida ishlatiladi.')
+alert(' Asosiy arxitektura xususiyatlari: dinamik terish, zaif terish, avtomatik xotirani boshqarish, prototip dasturlash, birinchi sinf ob\'ektlari sifatidagi funktsiyalar.')
+alert('JavaScript AQShda roʻyxatdan oʻtgan')
+alert('JavaScript - bu tillardan faqat bittasi bo\'ladi.')
---
+++ script/script.js
@@ -0,0 +1,46 @@
+"use strict";
+
+let age = prompt('2 + 2 nechi?')
+alert('agar siz 4 ga javob bergan bo\'lsangiz, demak siz haqsiz')
+
+
+prompt('9 ning kvadrat ildizi nima ?')
+alert('agar siz 3 ga javob bergan bo\'lsangiz, demak siz haqsiz')
+
+
+prompt('2 dan 5 gacha bo\'lgan daraja nima ? ')
+alert('agar siz 32 ga javob bergan bo\'lsangiz, demak siz haqsiz')
+
+const carName = "Tesla";
+// carName = "man";
+console.log(carName);
+const maxCarSpeed = "1000";
+// maxCarSpeed = "1000";
+console.log(maxCarSpeed);
+
+const carOwner = "Davronbek";
+console.log(carOwner);
+
+const What = "1-Question: What is the smallest country in the world by population?"
+console.log(What);
+
+const Answer = "Answer: Vatikan;"
+console.log(Answer);
+
+
+const Question2 = "2-Question: What is the largest country in the world by population?"
+console.log(Question2);
+
+const Answer2 = "Answer: China;"
+console.log(Answer2);
+
+const Question3 = "3-Question: What is the largest continent on the planet?;"
+console.log(Question3);
+
+const Answer3 = "Answer: Eurasia;" 
+console.log(Answer3);
+
+
+// const element = "Hay Elelement";
+
+// element = "Not Fount Error";
